82b65381df05867490c219cce715effd06d4fd72
Lenovo-User <Lenovo-User@DESKTOP-O3K5DUH>
2024-01-26 13:21:15 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace _2.C__Assessment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxWORD = new System.Windows.Forms.TextBox();
+            this.btnADD = new System.Windows.Forms.Button();
+            this.btnREMOVE = new System.Windows.Forms.Button();
+            this.btnCHANGE = new System.Windows.Forms.Button();
+            this.btnCLEAR = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnCLOSE = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 84);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a word or phrase";
+            // 
+            // txtBoxWORD
+            // 
+            this.txtBoxWORD.Location = new System.Drawing.Point(261, 81);
+            this.txtBoxWORD.Name = "txtBoxWORD";
+            this.txtBoxWORD.Size = new System.Drawing.Size(177, 26);
+            this.txtBoxWORD.TabIndex = 1;
+            // 
+            // btnADD
+            // 
+            this.btnADD.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnADD.Location = new System.Drawing.Point(41, 152);
+            this.btnADD.Name = "btnADD";
+            this.btnADD.Size = new System.Drawing.Size(82, 31);
+            this.btnADD.TabIndex = 2;
+            this.btnADD.Text = "ADD";
+            this.btnADD.UseVisualStyleBackColor = true;
+            this.btnADD.Click += new System.EventHandler(this.btnADD_Click);
+            // 
+            // btnREMOVE
+            // 
+            this.btnREMOVE.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnREMOVE.Location = new System.Drawing.Point(169, 152);
+            this.btnREMOVE.Name = "btnREMOVE";
+            this.btnREMOVE.Size = new System.Drawing.Size(105, 31);
+            this.btnREMOVE.TabIndex = 3;
+            this.btnREMOVE.Text = "REMOVE";
+            this.btnREMOVE.UseVisualStyleBackColor = true;
+            this.btnREMOVE.Click += new System.EventHandler(this.btnREMOVE_Click);
+            // 
+            // btnCHANGE
+            // 
+            this.btnCHANGE.Location = new System.Drawing.Point(178, 233);
+            this.btnCHANGE.Name = "btnCHANGE";
+            this.btnCHANGE.Size = new System.Drawing.Size(96, 31);
+            this.btnCHANGE.TabIndex = 4;
+            this.btnCHANGE.Text = "CHANGE";
+            this.btnCHANGE.UseVisualStyleBackColor = true;
+            this.btnCHANGE.Click += new System.EventHandler(this.btnCHANGE_Click);
+            // 
+            // btnCLEAR
+            // 
+            this.btnCLEAR.Location = new System.Drawing.Point(41, 233);
+            this.btnCLEAR.Name = "btnCLEAR";
+            this.btnCLEAR.Size = new System.Drawing.Size(82, 31);
+            this.btnCLEAR.TabIndex = 5;
+            this.btnCLEAR.Text = "CLEAR";
+            this.btnCLEAR.UseVisualStyleBackColor = true;
+            this.btnCLEAR.Click += new System.EventHandler(this.btnCLEAR_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(460, 72);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(199, 224);
+            this.listBox1.TabIndex = 6;
+            // 
+            // btnCLOSE
+            // 
+            this.btnCLOSE.Location = new System.Drawing.Point(769, 284);
+            this.btnCLOSE.Name = "btnCLOSE";
+            this.btnCLOSE.Size = new System.Drawing.Size(94, 36);
+            this.btnCLOSE.TabIndex = 7;
+            this.btnCLOSE.Text = "CLOSE";
+            this.btnCLOSE.UseVisualStyleBackColor = true;
+            this.btnCLOSE.Click += new System.EventHandler(this.btnCLOSE_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(1333, 692);
+            this.Controls.Add(this.btnCLOSE);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnCLEAR);
+            this.Controls.Add(this.btnCHANGE);
+            this.Controls.Add(this.btnREMOVE);
+            this.Controls.Add(this.btnADD);
+            this.Controls.Add(this.txtBoxWORD);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxWORD;
+        private System.Windows.Forms.Button btnADD;
+        private System.Windows.Forms.Button btnREMOVE;
+        private System.Windows.Forms.Button btnCHANGE;
+        private System.Windows.Forms.Button btnCLEAR;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnCLOSE;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.C__Assessment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnADD_Click(object sender, EventArgs e)
+        {
+            string newText = txtBoxWORD.Text.Trim();
+            if (!string.IsNullOrEmpty(newText))
+            {
+                listBox1.Items.Add(newText);
+                listBox1.Update();
+                txtBoxWORD.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a word or phrase.");
+            }
+        }
+
+        private void btnREMOVE_Click(object sender, EventArgs e)
+        {
+            // Iterate through the selected items and remove them from the ListBox
+            for (int i = listBox1.SelectedIndices.Count - 1; i >= 0; i--)
+            {
+                int selectedIndex = listBox1.SelectedIndices[i];
+                listBox1.Items.RemoveAt(selectedIndex);
+            }
+        }
+
+        private void btnCLEAR_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void btnCLOSE_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCHANGE_Click(object sender, EventArgs e)
+        {
+            // Check if an item is selected in the ListBox
+            if (listBox1.SelectedItem != null)
+            {
+                // Get the index of the selected item
+                int selectedIndex = listBox1.SelectedIndex;
+
+                // Get the new word from the TextBox
+                string newWord = txtBoxWORD.Text.Trim();
+
+                // Check if the new word is not empty
+                if (!string.IsNullOrEmpty(newWord))
+                {
+                    // Replace the selected item with the new word
+                    listBox1.Items[selectedIndex] = newWord;
+                    listBox1.Update();
+                    txtBoxWORD.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a new word.", "Empty Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a word to change.", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
